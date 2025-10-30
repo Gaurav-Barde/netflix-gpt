a7913c7a93c419ffeabb3a2697dec0feba7a3834
Gaurav-Barde <gabarde25@gmail.com>
2025-10-30 23:59:48 +0530

    created custom hook for Header component

---
+++ src/hooks/useHeader.ts
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { toggleShowGptSearch } from "../utils/redux/slices/gptSlice";
+import { setLanguage } from "../utils/redux/slices/languageSlice";
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "../utils/redux/appStore";
+import { useNavigate } from "react-router";
+import { onAuthStateChanged } from "firebase/auth";
+import { addUser, removeUser } from "../utils/redux/slices/userSlice";
+import { auth } from "../utils/firebaseConfig";
+
+const useHeader = () => {
+  const [isUserOptionsVisible, setIsUserOptionsVisible] = useState(false);
+  const user = useSelector(
+    (state: { user: { photoURL?: string } }) => state.user
+  );
+  const showGptSearch = useSelector(
+    (state: RootState) => state.gptSearch.showGptSearch
+  );
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(addUser({ uid, email, displayName, photoURL }));
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const userIconClickHandler = () => {
+    setIsUserOptionsVisible((prev) => !prev);
+  };
+
+  const gptSearchToggler = () => dispatch(toggleShowGptSearch());
+
+  const languageOptionChangeHandler = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    dispatch(setLanguage(e.target.value));
+  };
+};
